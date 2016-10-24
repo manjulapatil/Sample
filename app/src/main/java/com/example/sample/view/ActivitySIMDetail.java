2bcc6e983f3e376ab5152e327eb5c58e160ef3e4
manjulapatil <manjulapatil05@gmail.com>
2016-10-24 18:03:17 +0530

    commit

---
+++ app/src/main/java/com/example/sample/view/ActivitySIMDetail.java
@@ -0,0 +1,13 @@
+package com.example.sample.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitySIMDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_simdetail);
+    }
+}
